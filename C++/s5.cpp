e7ff11cbb931574c9f55cde556866e9479bd8774
gowthamtupili <gowthamreddytupili@gmail.com>
2023-02-11 16:39:50 +0530

    Fixed the bugs

--- C++/s5.cpp
+++ C++/s5.cpp
@@ -6,7 +6,7 @@ int main(void){
     string string1 = "Hello";
     string string2 = "Horld";
 
-    if (string1 == string2)
+    if (string1.compare(string2))
     cout << "Equal";
     else
     cout << "Not Equal";
